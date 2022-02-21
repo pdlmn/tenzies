c3b53626e8ec8cab50b4a8ffcbee750de7f9d4f7
pdlmn <djeinwings@gmail.com>
2022-02-21 18:53:56 +0200

    Added rollDice button

--- src/App.js
+++ src/App.js
@@ -1,17 +1,44 @@
+import { useState } from 'react'
+import uniqid from 'uniqid'
 import Die from './components/Die'
 
 const App = () => {
-  const createNewDice = () => {
-    const dice = []
-    for (let i = 0; i < 10; i++) {
-      dice.push(<Die value={1} />)
-    }
-    return dice
+  const [dice, setDice] = useState([])
+
+  const generateNumber = () => {
+    return Math.ceil(Math.random() * 6)
   }
+
+  const createNewDice = () => {
+    const newDice = []
+    for (let i = 0; i < 10; i++) {
+      newDice.push({
+        value: generateNumber(),
+        isHeld: false,
+        id: uniqid()
+      })
+    }
+    return newDice
+  }
+
+  const rollDice = () => {
+    setDice(createNewDice())
+  }
+
+  ;(function initDice() {
+    dice.length < 1 && setDice(createNewDice())
+  })()
+
+  const diceElements = dice.map(
+    (die) => <Die value={die.value} isHeld={die.isHeld} key={die.id} />
+  )
 
   return (
     <div className="container">
-      {createNewDice()}
+      <div className="die-container">
+        {diceElements}
+      </div>
+      <button className="btn" onClick={rollDice}>Roll dice</button>
     </div>
   );
 }
