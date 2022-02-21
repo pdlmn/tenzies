1daa5642b1d1d3dae43245c6fc26fd0ed08a4b0f
pdlmn <djeinwings@gmail.com>
2022-02-21 18:09:44 +0200

    Added css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
-function App() {
+import Die from './components/Die'
+
+const App = () => {
+  const createNewDice = () => {
+    const dice = []
+    for (let i = 0; i < 10; i++) {
+      dice.push(<Die value={1} />)
+    }
+    return dice
+  }
+
   return (
-    <div className="App">
-      Hello!
+    <div className="container">
+      {createNewDice()}
     </div>
   );
 }
---
+++ src/components/Die.js
@@ -0,0 +1,8 @@
+const Die = ({value}) => {
+  
+  return (
+    <div className="die">{value}</div>
+  )
+}
+
+export default Die
