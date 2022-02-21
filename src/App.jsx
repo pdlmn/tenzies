f233976dec0979801f972a0bd5daa595f46ad5b2
pdlmn <djeinwings@gmail.com>
2022-02-21 21:08:22 +0200

    Added win condition, new game button and react-confetti for win party

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import Confetti from 'react-confetti'
 import uniqid from 'uniqid'
 import Die from './components/Die'
 
 const App = () => {
   const [dice, setDice] = useState([])
+  const [win, setWin] = useState(false)
 
   const generateNumber = () => {
     return Math.ceil(Math.random() * 6)
@@ -37,6 +39,11 @@ const App = () => {
     ))
   }
 
+  const resetGame = () => {
+    setDice(createNewDice())
+    setWin(false)
+  }
+
   ;(function initDice() {
     dice.length < 1 && setDice(createNewDice())
   })()
@@ -44,19 +51,27 @@ const App = () => {
   const diceElements = dice.map(
     (die) => (
       <Die 
-        value={die.value} 
+        value={die.value}
         isHeld={die.isHeld}
         key={die.id}
         handleClick={() => holdDie(die.id)}
       />
     ))
 
+  useEffect(() => {
+    const isWon = dice.every((die) => die.isHeld && die.value === dice[0].value)
+    if (isWon) setWin(true)
+  }, [dice])
+
   return (
     <div className="container">
+      {win && <Confetti numberOfPieces={600} recycle={false}/>}
       <div className="die-container">
         {diceElements}
       </div>
-      <button className="btn" onClick={rollDice}>Roll dice</button>
+      <button className="btn" onClick={win ? resetGame : rollDice}>
+        {win ? "New game" : "Roll dice"}
+      </button>
     </div>
   );
 }
